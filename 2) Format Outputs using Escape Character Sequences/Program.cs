379d7da7392762f1084a2d88476022b770be774b
Yeabsira <103306411+Yeab243@users.noreply.github.com>
2023-09-21 23:13:00 -0700

    Update Program.cs

--- 2) Format Outputs using Escape Character Sequences/Program.cs
+++ 2) Format Outputs using Escape Character Sequences/Program.cs
@@ -1,1 +1,52 @@
+string studentName = "Halima Oman";
+string course1Name = "PHIL 309: Ethics and Public Policy";
+string course2Name = "PHIL 303: Environmental Ethics";
+string course3Name = "PHIL 202: Political Philosophy";
+string course4Name = "PHIL 101: Introduction to Philosophy";
+string course5Name = "PHIL 201: Moral Philosophy";
+
+int course1Credit = 5;
+int course2Credit = 4;
+int course3Credit = 3;
+int course4Credit = 4;
+int course5Credit = 5;
+
+int gradeA = 4;
+int gradeB = 3;
+
+int course1Grade = gradeA;
+int course2Grade = gradeB;
+int course3Grade = gradeB;
+int course4Grade = gradeB;
+int course5Grade = gradeA;
+
+int totalCreditHours = 0;
+totalCreditHours += course1Credit;
+totalCreditHours += course2Credit;
+totalCreditHours += course3Credit;
+totalCreditHours += course4Credit;
+totalCreditHours += course5Credit;
+
+int totalGradePoints = 0;
+totalGradePoints += course1Credit * course1Grade;
+totalGradePoints += course2Credit * course2Grade;
+totalGradePoints += course3Credit * course3Grade;
+totalGradePoints += course4Credit * course4Grade;
+totalGradePoints += course5Credit * course5Grade;
+
+decimal gradePointAverage = (decimal) totalGradePoints/totalCreditHours;
+
+int leadingDigit = (int) gradePointAverage;
+int trailingDigits = (int) (gradePointAverage * 100) - (leadingDigit * 100);
+
+Console.WriteLine($"Student: {studentName}\n");
+Console.WriteLine("Course\t\t\t\tGrade\tCredit Hours");
+
+Console.WriteLine($"{course1Name}\t\t\t{course1Grade}\t\t{course1Credit}");
+Console.WriteLine($"{course2Name}\t\t\t{course2Grade}\t\t{course2Credit}");
+Console.WriteLine($"{course3Name}\t\t\t{course3Grade}\t\t{course3Credit}");
+Console.WriteLine($"{course4Name}\t{course4Grade}\t\t{course4Credit}");
+Console.WriteLine($"{course5Name}\t\t{course5Grade}\t\t{course5Credit}");
+
+Console.WriteLine($"\nFinal GPA:\t\t\t{leadingDigit}.{trailingDigits}");
 
