ac84b37d4d851b4ddbe02d765939c59393a104d5
Yeabsira Girma <103306411+Yeab243@users.noreply.github.com>
2023-10-17 23:43:37 -0700

    Update Program.cs

--- 7) Find the average of even numbers between 2 user-specified bounds/Program.cs
+++ 7) Find the average of even numbers between 2 user-specified bounds/Program.cs
@@ -1,1 +1,88 @@
+using System;
 
+class Program
+{
+  static void Main()
+  {
+    int lowerBound, upperBound;
+    decimal averageValue = 0;
+    bool exit = false;
+
+    do
+    {
+      // Prompt the user for the lower and upper bounds
+      Console.Write("Enter the lower bound: ");
+      lowerBound = int.Parse(Console.ReadLine());
+
+      Console.Write("Enter the upper bound: ");
+      upperBound = int.Parse(Console.ReadLine());
+
+      try
+      {
+        // Calculate the sum of the even numbers between the bounds
+        averageValue = CalculateAverageOfEvenNumbers(lowerBound, upperBound);
+
+        // Display the result to the user
+        Console.WriteLine($"The average of even numbers between {lowerBound} and {upperBound} is {averageValue}.");
+
+        exit = true; // Set exit flag to exit the loop
+      }
+      catch (ArgumentOutOfRangeException ex)
+      {
+        HandleInvalidInput(ex);
+      }
+    } while (!exit); 
+  }
+
+  static decimal CalculateAverageOfEvenNumbers(int lowerBound, int upperBound)
+  {
+    // Check if the lower bound is greater than or equal to the upper bound
+    if (lowerBound >= upperBound)
+    {
+      throw new ArgumentOutOfRangeException("upperBound", "ArgumentOutOfRangeException: upper bound must be greater than lower bound.");
+    }
+
+    int sum = 0;
+    int count = 0;
+    decimal average = 0;
+
+    for (int i = lowerBound; i <= upperBound; i++)
+    {
+      if (i % 2 == 0)
+      {
+        sum += i;
+        count++;
+      }
+    }
+
+    average = (decimal)sum / count;
+
+    return average;
+  }
+
+  static void HandleInvalidInput(ArgumentOutOfRangeException ex)
+  {
+    Console.WriteLine("An error has occurred.");
+    Console.WriteLine(ex.Message);
+    Console.WriteLine($"The upper bound must be greater than {lowerBound}");
+    Console.Write("Enter a new upper bound (or enter 'Exit' to quit): ");
+    string userResponse = Console.ReadLine();
+
+    if (userResponse.Equals("Exit", StringComparison.OrdinalIgnoreCase))
+    {
+      exit = true; // User chose to exit the loop
+    }
+    else
+    {
+      // Attempt to parse the user input as the new upper bound
+      if (int.TryParse(userResponse, out int newUpperBound))
+      {
+        upperBound = newUpperBound; 
+      }
+      else
+      {
+        Console.WriteLine("Invalid input. Please enter a valid integer."); /
+      }
+    }
+  }
+}
